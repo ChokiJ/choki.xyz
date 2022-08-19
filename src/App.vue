19ca44d187f4727e2e7e934f7b1e287e9aabbc69
Chris <chokij@outlook.com>
2022-08-19 16:25:45 +0800

    update
    - 字体加载完成后再渲染页面
    - 更换为Playfair 2字体
    - 新增标题根据鼠标移动变化
    
    Signed-off-by: Chris <chokij@outlook.com>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HomePageVue from "./components/HomePage.vue";
 import HomePage from "./components/HomePage.vue";
 </script>
 
@@ -11,14 +10,19 @@ import HomePage from "./components/HomePage.vue";
 
 <style>
 @font-face {
-  font-family: "Playfair Display Regular";
-  src: local("Playfair Display Regular"),
-    url(./fonts/PlayfairDisplay-VariableFont_wght.ttf);
+    font-family: "Playfair Display Regular";
+    src: local("Playfair Display Regular"),
+        url(./fonts/PlayfairRomanVF.ttf);
 }
 
 @font-face {
-  font-family: "Playfair Display Italic";
-  src: local("Playfair Display Italic"),
-    url(./fonts/PlayfairDisplay-Italic-VariableFont_wght.ttf);
+    font-family: "Playfair Display Italic";
+    src: local("Playfair Display Italic"),
+        url(./fonts/PlayfairDisplay-Italic-VariableFont_wght.ttf);
+}
+
+#app {
+    height: 100%;
+    width: 100%;
 }
 </style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,24 +1,44 @@
 <script setup>
+import FontFaceObserver from "fontfaceobserver";
+import { ref, onMounted, reactive } from 'vue';
+
+// 字体加载完成后再渲染页面
+const font1 = new FontFaceObserver("Playfair Display Regular");
+const font2 = new FontFaceObserver("Playfair Display Italic");
+const loaded = ref(false);
+Promise.all([font1.load(), font2.load()]).then(function () {
+    loaded.value = true;
+});
+
+// 鼠标移动更改标题字重
+onMounted(() => { window.addEventListener('mousemove', handleMouseMove) })
+const handleMouseMove = e => {
+    changeWeight(e.clientX, e.clientY);
+}
+const changeWeight = (x, y) => {
+    const weight = x * 0.6;
+    const opsz = y * 2;
+    document.querySelector('#title').style.fontVariationSettings = `'wght' ${weight},'opsz' ${opsz}`;
+}
 </script>
 
 <template>
-    <div class="box">
-        <h1 title>Chris Jia</h1>
-        <a class="subtitle">is a student and a designer</a>
-        <div class="main">
-            You can find me on
-            <a class="link" href="https://weibo.com/u/6506391030">weibo</a> and
-            <a class="link" href="https://www.instagram.com/choki.redfield/">instagram</a>,
-            and see my works on
-            <a
-                class="link"
-                href="https://www.behance.net/Chokijf4b3"
-            >Behance</a>.
-            <br />Know me more at my
-            <a class="link" href="https://blog.choki.xyz">
-                <span>blog</span>
-            </a>. Contact me via
-            <a class="link" href="mailto:chokij@outlook.com">email</a>.
+    <div class="container" v-if="loaded">
+        <div class="box">
+            <h1 id="title">Chris Jia</h1>
+            <div class="subtitle">is a student and a designer</div>
+            <div class="main">
+                You can find me on
+                <a class="link" href="https://weibo.com/u/6506391030">weibo</a> and
+                <a class="link" href="https://www.instagram.com/choki.redfield/">instagram</a>,
+                and see my works on
+                <a class="link" href="https://www.behance.net/Chokijf4b3">Behance</a>.
+                <br />Know me more at my
+                <a class="link" href="https://blog.choki.xyz">
+                    <span>blog</span>
+                </a>. Contact me via
+                <a class="link" href="mailto:chokij@outlook.com">email</a>.
+            </div>
         </div>
     </div>
 </template>
@@ -27,7 +47,6 @@
 h1 {
     font-family: Playfair Display Regular;
     font-style: normal;
-    font-weight: 900;
     font-size: 96px;
     line-height: 128px;
     text-align: center;
@@ -36,22 +55,16 @@ h1 {
     margin-top: 0px;
     -webkit-animation: 2s ease h1anime;
     animation: 2s ease h1anime;
+    font-variation-settings: 'opsz' 1000, 'wght' 900;
 }
 
-@-webkit-keyframes h1anime {
-    from {
-        font-variation-settings: "wght" 400;
-    }
-    to {
-        font-variation-settings: "wght" 900;
-    }
-}
 @keyframes h1anime {
     from {
-        font-variation-settings: "wght" 400;
+        font-variation-settings: "wght" 100, 'opsz' 100;
     }
+
     to {
-        font-variation-settings: "wght" 900;
+        font-variation-settings: "wght" 900, 'opsz' 1000;
     }
 }
 
@@ -76,18 +89,26 @@ h1 {
     display: block;
     text-align: center;
     padding-top: 1rem;
+    font-variation-settings: 'opsz' 1000;
 }
+
 .link {
     font-family: Playfair Display Italic;
     font-style: normal;
     font-weight: 500;
     font-size: 26px;
     line-height: 36px;
-    color: #2c3e50;
 }
-.box {
+
+.container {
     display: block;
-    width: 70vw;
+    width: 100%;
+    height: 100%;
+}
+
+.box {
+    position: relative;
+    top: 30%;
 }
 
 body {
@@ -99,5 +120,15 @@ body {
     background-color: rgb(250, 246, 243);
     justify-content: center;
     align-items: center;
+}
+
+a {
+    color: #2c3e50;
+    transition: all 0.3s ease;
+}
+
+a:hover {
+    background-color: #2c3e50;
+    color: #ffffff;
 }
 </style>
